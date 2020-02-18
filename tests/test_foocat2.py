24cf2bb37ad340bd994bbc0a90e4d624d6e67a02
ttimbers <tiffany.timbers@stat.ubc.ca>
2020-02-18 15:11:32 -0800

    fixing test

--- tests/test_foocat2.py
+++ tests/test_foocat2.py
@@ -7,6 +7,6 @@ import pandas as pd
 def test_catbind():
   a = pd.Categorical(["character", "hits", "your", "eyeballs"])
   b = pd.Categorical(["but", "integer", "where it", "counts"])
-  assert ((cat.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
-  assert ((cat.catbind(a, b)).categories == ["but", "character", "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
+  assert ((foocat.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+  assert ((foocat.catbind(a, b)).categories == ["but", "character", "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
 
