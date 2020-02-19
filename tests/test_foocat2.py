9f33ef958525b39dca250325e5438e13a10b76e1
ttimbers <tiffany.timbers@stat.ubc.ca>
2020-02-19 14:07:05 -0800

    fixed style

--- tests/test_foocat2.py
+++ tests/test_foocat2.py
@@ -1,12 +1,10 @@
-import pytest
-
 from foocat2 import foocat2
-
 import pandas as pd
 
-def test_catbind():
-  a = pd.Categorical(["character", "hits", "your", "eyeballs"])
-  b = pd.Categorical(["but", "integer", "where it", "counts"])
-  assert ((foocat2.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
-  assert ((foocat2.catbind(a, b)).categories == ["but", "character", "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
 
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((foocat2.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((foocat2.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
